26ec36bb933b33467a9f3330b6dcd1ffa59a7328
HEZRON NYAMBOGA <133749469+nyambogahezron@users.noreply.github.com>
2024-01-22 01:36:03 +0300

    Fix App.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 import {
   createBrowserRouter,
   createRoutesFromElements,
